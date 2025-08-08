ed04a608b082da6b4c7410b693364233ac8b85a2
karub360 <karub360@gmail.com>
2025-08-08 14:56:18 +0300

    Add side navigation with SplitView

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace TSE_Automation.Views;
 
@@ -7,5 +8,28 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void MenuToggleButton_Click(object? sender, RoutedEventArgs e)
+    {
+        MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
+    }
+
+    private void DeneyEkleButton_Click(object? sender, RoutedEventArgs e)
+    {
+        var deneyEkleView = new DeneyEkleView();
+        MainContentControl.Content = deneyEkleView;
+    }
+
+    private void DeneyRaporlaButton_Click(object? sender, RoutedEventArgs e)
+    {
+        var deneyRaporlaView = new DeneyRaporlaView();
+        MainContentControl.Content = deneyRaporlaView;
+    }
+
+    private void DeneySilDuzenleButton_Click(object? sender, RoutedEventArgs e)
+    {
+        var deneySilDuzenleView = new DeneySilDuzenleView();
+        MainContentControl.Content = deneySilDuzenleView;
     }
 }
